c7143136e0b315582d8825eb7c1ae4d28b05ff7d
dongyeuk <yupab3@naver.com>
2025-08-07 12:44:48 +0900

    upload define

---
+++ define/define-test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+#define REFLECT_RATIO 1.3
+
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    string str = "REFLECT_RATIO : " + REFLECT_RATIO;
+    return 0;
+}
